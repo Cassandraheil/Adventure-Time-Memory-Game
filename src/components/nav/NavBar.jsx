db9476eefe61b5148a0c146d2982b0d036c7f145
Cassandra Heil <Cassandraheil19@gmail.com>
2020-07-24 14:17:27 -0400

    updated class to classname, installed serve

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -5,13 +5,13 @@ import "./style.css"
 function NavBar(props) {
   return (
     <div>
-      <nav class="navbar navbar-inverse navbar-fixed-top jumbotron">
-      <img class="adventureImg" alt="adventure time logo" src="https://i.etsystatic.com/10281372/r/il/7af34c/1047011185/il_570xN.1047011185_og43.jpg" />
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <h3 class="navbar-brand">Memory Game</h3>
+      <nav className="navbar navbar-inverse navbar-fixed-top jumbotron">
+      <img className="adventureImg" alt="adventure time logo" src="https://i.etsystatic.com/10281372/r/il/7af34c/1047011185/il_570xN.1047011185_og43.jpg" />
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <h3 className="navbar-brand">Memory Game</h3>
           </div>
-          <div class="nav navbar-nav">
+          <div className="nav navbar-nav">
             <p>Score: {props.score}</p>
             <p>Top Score: {props.topScore}</p>
           </div>
